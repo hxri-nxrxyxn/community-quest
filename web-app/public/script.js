040c18428493cbbd5d23266d0c54cd0fae67fa75
Hari Narayan <harinarayanmr@aol.com>
2024-11-08 07:04:51 +0530

    trying to link database

--- web-app/public/script.js
+++ web-app/public/script.js
@@ -1,11 +1,13 @@
 const inputName = document.querySelector('.register__name');
 const inputPassword = document.querySelector('.register__password');
 const inputEmail = document.querySelector('.register__email');
+const submit = document.querySelector('.btn-login')
 
 //add event  
 submit.addEventListener('click',async () => {
+    console.log("clicked")
     try {
-        const response = await fetch('http://localhost:5000/api/users/addevent', {
+        const response = await fetch('http://localhost:5000/api/users/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({"email" : inputEmail.value, "password" : inputPassword.value})
@@ -13,8 +15,10 @@ submit.addEventListener('click',async () => {
         const data = await response.json();
         if (response.ok) {
             alert('Login successful!');
+            console.log('Login successful!');
         } else {
             alert(data.error);
+            console.log(data.error);
         }
     } catch (error) {
         console.error('Error:', error);
